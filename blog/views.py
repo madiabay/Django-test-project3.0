896cc4463079820564a28c8e2571610ec3a5d4af
Mbappe <imbappe@bk.ru>
2022-10-24 14:43:37 +0600

    Post by one

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 # Create your views here.
 
@@ -28,12 +29,16 @@ class PostsByCategory(ListView):
         context['title'] = Category.objects.get(slug=self.kwargs['slug'])
         return context
 
+class PostsByTag(ListView):
+    pass
 
-def index(request):
-    return render(request, 'blog/index.html')
+class GetPost(DetailView):
+    model = Post
+    context_object_name = 'post'
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
-
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
