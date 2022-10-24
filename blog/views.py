a3fba9bbaa561ac7a1f337e0e91dcaf06e8c23f3
Mbappe <imbappe@bk.ru>
2022-10-24 20:32:45 +0600

    view tag's with posts

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,17 @@ class PostsByCategory(ListView):
         return context
 
 class PostsByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 class GetPost(DetailView):
     model = Post
